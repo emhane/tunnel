7e1c4dfaecac71d471ff891754a82d1f8680dac2
Emilia Hane <emilia.hane@ethereum.org>
2023-04-25 16:57:06 +0200

    Crypto bug fix - place holder crytpo reuse same cipher to show the point

--- src/session/crypto.rs
+++ src/session/crypto.rs
@@ -4,7 +4,6 @@ use aes_gcm::{
     Aes128Gcm, Error as AesGcmError, KeyInit,
 };
 use rand;
-use zeroize::Zeroize;
 
 /// Length of [`aes_gcm::Aes128Gcm`] key in bytes.
 pub const KEY_AES_GCM_128_LENGTH: usize = 32;
@@ -15,40 +14,36 @@ pub const TAG_AES_GCM_128_LENGTH: usize = 16;
 
 /// A message nonce used in encryption.
 pub type NonceAesGcm128 = [u8; NONCE_AES_GCM_128_LENGTH];
-/// Key used in [`aes_gcm::Aes128Gcm`] encryption.
-pub type KeyAesGcm128 = [u8; KEY_AES_GCM_128_LENGTH];
 /// Additional associated data used in authenticating the encryption of tunnel packet.
 pub type Aad = [u8; HEADER_LENGTH];
 
-/// Key used in [`aes_gcm::Aes128Gcm`] encryption.
-#[derive(Zeroize, PartialEq)]
-pub struct EncryptionKey(KeyAesGcm128);
+/// Cipher used in [`aes_gcm::Aes128Gcm`] encryption.
+pub struct Cipher(Aes128Gcm);
 
-impl EncryptionKey {
+impl Cipher {
     pub fn new() -> Self {
         let encryption_key: [u8; KEY_AES_GCM_128_LENGTH] = rand::random();
-        EncryptionKey(encryption_key)
+        let cipher = Aes128Gcm::new(GenericArray::from_slice(&encryption_key));
+        Cipher(cipher)
     }
 
     pub fn aes_gcm_128_encrypt(
-        &self,
+        &mut self,
         nonce: &NonceAesGcm128,
         msg: &[u8],
         aad: &Aad,
     ) -> Result<Vec<u8>, AesGcmError> {
-        let mut cipher = Aes128Gcm::new(GenericArray::from_slice(&self.0));
         let payload = Payload { msg, aad };
-        cipher.encrypt(GenericArray::from_slice(nonce), payload)
+        self.0.encrypt(GenericArray::from_slice(nonce), payload)
     }
 
     pub fn aes_gcm_128_decrypt(
-        &self,
+        &mut self,
         nonce: &NonceAesGcm128,
         msg: &[u8],
         aad: &Aad,
     ) -> Result<Vec<u8>, AesGcmError> {
-        let mut cipher = Aes128Gcm::new(GenericArray::from_slice(&self.0));
         let payload = Payload { msg, aad };
-        cipher.decrypt(GenericArray::from_slice(nonce), payload)
+        self.0.decrypt(GenericArray::from_slice(nonce), payload)
     }
 }
--- src/session/mod.rs
+++ src/session/mod.rs
@@ -7,31 +7,29 @@ use rand;
 
 mod crypto;
 
-pub use crypto::{
-    Aad, EncryptionKey, NonceAesGcm128, NONCE_AES_GCM_128_LENGTH, TAG_AES_GCM_128_LENGTH,
-};
+pub use crypto::{Aad, Cipher, NonceAesGcm128, NONCE_AES_GCM_128_LENGTH, TAG_AES_GCM_128_LENGTH};
 
 /// A Session holds the key chain used to encrypt a [`TunnelPacket`], it uses the same encryption
 /// algorithm as discv5.
 pub struct Session {
     /// The key used to encrypt/decrypt messages. Upon starting a session, the key will be passed
     /// from one peer to the other in an encrypted discv5 session using the TALKREQ message.
-    key: EncryptionKey,
+    cipher: Cipher,
     /// If a new session is being established using discv5 TALKREQ and TALKRESP, the older key
     /// is maintained as race conditions in the key sharing can give different views of which
     /// keys are canon. The key that worked to decrypt our last message (or are freshly
     /// established) exist in `key` and the previous key is optionally stored in `old_key`. We
     /// attempt to decrypt messages with `key` before optionally trying `old_key`.
-    old_key: Option<EncryptionKey>,
+    old_key: Option<Cipher>,
     /// Number of messages sent. Used to ensure the nonce used in message encryption is always
     /// unique.
     counter: u32,
 }
 
 impl Session {
-    pub fn new(keys: EncryptionKey) -> Self {
+    pub fn new(keys: Cipher) -> Self {
         Session {
-            key: keys,
+            cipher: keys,
             old_key: None,
             counter: 0,
         }
@@ -40,7 +38,7 @@ impl Session {
     /// A new session has been established. Update the session keys.
     pub fn update(&mut self, new_session: Session) {
         // Optimistically assume the new keys are canonical.
-        self.old_key = Some(std::mem::replace(&mut self.key, new_session.key));
+        self.old_key = Some(std::mem::replace(&mut self.cipher, new_session.cipher));
     }
 
     /// Uses the current `Session` to encrypt a message. Encrypt packets with the current session
@@ -64,7 +62,7 @@ impl Session {
         let mut aad = [0u8; HEADER_LENGTH];
         aad[..CONNECTION_ID_LENGTH].copy_from_slice(&connection_id);
         aad[CONNECTION_ID_LENGTH..].copy_from_slice(&nonce);
-        let cipher_text = self.key.aes_gcm_128_encrypt(&nonce, msg, &aad)?;
+        let cipher_text = self.cipher.aes_gcm_128_encrypt(&nonce, msg, &aad)?;
 
         let header = TunnelPacketHeader(connection_id, nonce);
 
@@ -81,14 +79,14 @@ impl Session {
         aad: &Aad,
     ) -> Result<Vec<u8>, SessionError> {
         // First try with the canonical keys.
-        match self.key.aes_gcm_128_decrypt(&nonce, msg, aad) {
+        match self.cipher.aes_gcm_128_decrypt(&nonce, msg, aad) {
             Ok(decrypted) => Ok(decrypted),
             Err(e) => {
                 // If these keys did not work, try old_keys and
-                if let Some(old_keys) = self.old_key.take() {
-                    let decrypted_old_keys = old_keys.aes_gcm_128_decrypt(&nonce, msg, aad)?;
+                if let Some(mut old_cipher) = self.old_key.take() {
+                    let decrypted_old_keys = old_cipher.aes_gcm_128_decrypt(&nonce, msg, aad)?;
                     // rotate the keys
-                    self.old_key = Some(std::mem::replace(&mut self.key, old_keys));
+                    self.old_key = Some(std::mem::replace(&mut self.cipher, old_cipher));
                     return Ok(decrypted_old_keys);
                 }
                 return Err(SessionError::EncryptionError(e));
